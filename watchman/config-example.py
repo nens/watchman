af3202d6b7812d6604746fb5381c63849d4aff9c
byrman <carsten.byrman@nelen-schuurmans.nl>
2016-02-23 16:29:11 +0100

    Provide a better config example.

--- watchman/config-example.py
+++ watchman/config-example.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
-"""
 
+import os
 
-"""
+SETTINGS_DIR = os.path.dirname(os.path.realpath(__file__))
+BUILDOUT_DIR = os.path.abspath(os.path.join(SETTINGS_DIR, '..'))
 
 LOGGING = {
     'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+            'level': 'DEBUG',
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(
+                BUILDOUT_DIR, 'var', 'log', 'watchman.log'),
+            'formatter': 'verbose',
+            'level': 'INFO',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+        'watchman.notify': {
+            'propagate': True,
+        },
+    },
 }
 
 # https://docs.python.org/2/library/fnmatch.html
@@ -15,5 +44,13 @@ PATTERNS = {
 
 # http://seb-m.github.io/pyinotify/
 WATCHES = [
-    {'path': '/tmp'},
+    {'path': '/foo'},
+    {'path': '/bar'},
 ]
+
+# Files may be moved beforehand.
+# This is optional.
+MOVES = {
+    '/foo': '/isilon/foo',
+    '/bar': '/isilon/bar',
+}
